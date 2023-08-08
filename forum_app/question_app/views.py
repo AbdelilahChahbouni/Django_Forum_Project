074dd60e8f739b8ae257d38cd0ac5165c9e3333f
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-08-08 22:49:01 +0100

    add question details with tags

--- forum_app/question_app/views.py
+++ forum_app/question_app/views.py
@@ -8,20 +8,22 @@ from .forms import QuestionForm
 
 def list_questions(request):
     d = Questions.objects.all().values()
-    #data = Questions.tags.all()
     data2 = Questions.tags.values_list('name' , flat=True)
-    new_data = zip(d , data2)
+    #new_data = zip(d , data2)
+    my_loop=range(1, 8)
 
-    return render(request,"list_questions.html" , {'que' : d , 'new_data': new_data , 'data2': data2})
+    return render(request,"list_questions.html" , {'que' : d , 'tags': data2 , 'test':my_loop})
 
     
 def add_question(request):
     if request.method == "POST":
         form = QuestionForm(request.POST , request.FILES)
         if form.is_valid():
-            new_form = form.save(commit=False)
-            new_form.author = request.user
-            new_form.save()
+            #new_form = form.save(commit=False)
+            #new_form.author = request.user
+            #new_form.save()
+            form.instance.author = request.user
+            form.save()
             return redirect("home")
     else:
         form = QuestionForm()
@@ -30,7 +32,8 @@ def add_question(request):
 
 def question_details(request , q_id):
     question = Questions.objects.filter(pk=q_id)
-    return render(request , 'question_details.html' , {'question':question})
+    data2 = Questions.tags.values_list('name' , flat=True)[q_id-1]
+    return render(request , 'question_details.html' , {'question':question , 'tags': data2})
 
     
 
